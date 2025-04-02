39103a4a7ed7adfa1d27d5c33620298981593c4a
Theodor Ampas <71594408+AmpasTheodoros@users.noreply.github.com>
2025-04-02 20:51:23 +0300

    update analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { inject } from '@vercel/analytics'; // ✅ Must be at the top
+inject(); 
+
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById("root")!).render(<App />);
