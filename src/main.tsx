2e6f16d2d88546653595c6f6cf02fadcb3a5e138
Theodor Ampas <71594408+AmpasTheodoros@users.noreply.github.com>
2025-04-07 13:55:03 +0300

    google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,30 @@ import clarity from "@microsoft/clarity";
 clarity.init(import.meta.env.VITE_CLARITY_ID);
 
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+declare global {
+    interface Window {
+      dataLayer: any[];
+      gtag: (...args: any[]) => void;
+    }
+}
+
+if (typeof window !== 'undefined') {
+    const script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-QZ593Y8PM1';
+    script.async = true;
+    document.head.appendChild(script);
+  
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function (...args) {
+      window.dataLayer.push(args);
+    };
+  
+    window.gtag('js', new Date());
+    window.gtag('config', 'G-QZ593Y8PM1');
+  }
+
+  createRoot(document.getElementById("root")!).render(<App />);
+  
